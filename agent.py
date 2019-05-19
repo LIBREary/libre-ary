bf4513bee387447e6575c2c32f0c5c9a8fc74467
Ben Glick <glick@lclark.edu>
2019-05-18 18:17:57 -0700

    working on agent

--- agent.py
+++ agent.py
@@ -1,9 +1,14 @@
+from adapters import *
+from ingester import Ingester
 
+adapter_translate = {
+	"local": LocalAdapter.LocalAdapter
+}
 
 class LibrearyAgent:
 
 
-	def __init__():
+	def __init__(self, config):
 		self.metadata_db = os.path.realpath(config['metadata'].get("db_file"))
         self.conn = sqlite3.connect(self.metadata_db)
         self.cursor = self.conn.cursor()
@@ -11,21 +16,22 @@ class LibrearyAgent:
         self.dropbox_dir = config["options"]["dropbox_dir"]
         self.adapter_type = "local"
         self.ret_dir = config["options"]["output_dir"]
+        self.ingester = Ingester(config=self.config)
 
 
-    def run_check():
+    def run_check(self):
     	pass
 
 
-    def get_all_copies(r_id):
+    def get_all_copies(self, r_id):
     	pass
 
 
-    def ingest_to_db(file):
+    def ingest_to_db(self, file):
     	pass
 
-    def create_adapter(adapter_id):
-    	pass
+    def create_adapter(self, adapter_type, adapter_id):
+    	adapter = adapter_translate[adapter_type](config=self.config["adapters"][adapter_id])
 
 
 
@@ -41,7 +47,8 @@ if __name__ == '__main__':
 
         "local1": {
             "storage_dir": "~/Desktop/librelocal",
-            "adapter_identifier": "local1"
+            "adapter_identifier": "local1",
+            "adapter_type": "local"
         },
         "local2": {
         	"storage_dir": "~/Desktop/librelocal2",
