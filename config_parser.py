6075e4d3b9a9fd7ea61d3d63389e9862e01db8c9
Ben Glick <glick@lclark.edu>
2019-05-25 21:27:49 -0500

    continuing with config parser

--- config_parser.py
+++ config_parser.py
@@ -1,26 +1,34 @@
 import json
+import os
 
 CONFIG_DIR = "config"
 
 class ConfigParser:
 
 
-	def __init__(self):
-		self.metadata_db = os.path.realpath(config['metadata'].get("db_file"))
-        self.conn = sqlite3.connect(self.metadata_db)
-        self.cursor = self.conn.cursor()
+    def __init__(self):
+        #self.metadata_db = os.path.realpath(config['metadata'].get("db_file"))
+        #self.conn = sqlite3.connect(self.metadata_db)
+        #self.cursor = self.conn.cursor()
+        pass
 
-	def create_config_for_adapter(self, adapter_id):
-		base_config = json.read(open("{}/{}_config.json".format(CONFIG_DIR, adapter_id)))
-		general_config = json.read(open("{}/agent_config.json".format(CONFIG_DIR)))
-		full_adapter_conf = {}
-		full_adapter_conf["adapter"] = base_config
-		full_adapter_conf["metadata"] = general_config["metadata"]
-		full_adapter_conf["options"] = general_config["options"]
+    def create_config_for_adapter(self, adapter_id, adapter_type):
+        base_config = json.load(open("{}/{}_config.json".format(CONFIG_DIR, adapter_id)))
+        general_config = json.load(open("{}/agent_config.json".format(CONFIG_DIR)))
+        full_adapter_conf = {}
+        full_adapter_conf["adapter"] = base_config["adapter"]
+        full_adapter_conf["adapter"]["adapter_type"] = adapter_type
+        full_adapter_conf["metadata"] = general_config["metadata"]
+        full_adapter_conf["options"] = general_config["options"]
 
-		return full_adapter_conf
+        return full_adapter_conf
 
 
-	def add_new_adapter(self, adapter_config):
-		pass
+    def add_new_adapter(self, adapter_config):
+        pass
 
+
+if __name__ == '__main__':
+    parser = ConfigParser()
+
+    print(parser.create_config_for_adapter("local1", "local"))
