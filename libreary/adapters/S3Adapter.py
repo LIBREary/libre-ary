dc9e1d71b70623222fed7c92ac79d92165d841be
Ben Glick <glick@lclark.edu>
2019-12-29 14:45:19 -0600

    adding S3 adapter. This closes #13

--- libreary/adapters/S3Adapter.py
+++ libreary/adapters/S3Adapter.py
@@ -1,6 +1,7 @@
 import json
 import os
 import sqlite3
+import hashlib
 
 try:
     import boto3
@@ -19,6 +20,8 @@ class S3Adapter:
         """
         This should handle configuration (auth, etc.) and set up the
         metadata db connection
+
+        You must have already created the S3 bucket for this to work.
         """
         self.config = config
 
@@ -54,9 +57,10 @@ class S3Adapter:
         """Initialize the boto client."""
 
         self.session = self.create_session()
-        self.client = self.session.client('s3')
-        self.s3 = self.session.resource('s3')
-
+        #self.client = self.session.client('s3')
+        #self.s3 = self.session.resource('s3')
+        self.client = boto3.client('s3')
+        self.s3 = boto3.resource('s3')
 
     def create_session(self):
         """Create a session.
@@ -104,24 +108,88 @@ class S3Adapter:
 
     def store(resource_id):
         """
-        Given a resource id, saves resource and returns confirmation
-        """
-        pass
+        Given a resource id, saves resource and returns confirmation.
 
-    def _store_canonical(current_path, r_id, delete_after_store=False):
+        Assumes file is stored as `filename` in `dropbox_dir`. AdapterManager will verify this
+        """
+        file_metadata = self.load_metadata(r_id)[0]
+        dropbox_path = file_metadata[2]
+        checksum = file_metadata[4]
+        name = file_metadata[1]
+        current_location = "{}/{}".format(self.dropbox_dir, dropbox_path)
+
+        sha1Hash = hashlib.sha1(open(current_location,"rb").read())
+        sha1Hashed = sha1Hash.hexdigest()
+
+
+        other_copies = self.cursor.execute(
+            "select * from copies where resource_id='{}' and adapter_identifier='{}' and not canonical = 1 limit 1".format(
+            r_id, self.adapter_id)).fetchall()
+        if len(other_copies) != 0:
+            print("Other copies from this adapter exist")
+            raise StorageFailedException
+
+        if sha1Hashed == checksum:
+            locator = '{}_{}'.format(filename, resource_id)
+            self.s3.Bucket(self.bucket_name).upload_file(current_path, locator)
+        else:
+            print("Checksum Mismatch")
+            raise Exception
+
+        
+
+        self.cursor.execute(
+            "insert into copies values ( ?,?, ?, ?, ?, ?, ?)",
+            [None, r_id, self.adapter_type, locator, sha1Hashed, self.adapter_id, False])
+        self.conn.commit()
+
+    def _store_canonical(self, current_path, r_id, checksum, filename):
         """
         Ingest files as the canonical adapter. Don't run this function. Ingester will call it.
         """
-        pass
+        current_location = current_path
+
+        sha1Hash = hashlib.sha1(open(current_location,"rb").read())
+        sha1Hashed = sha1Hash.hexdigest()
+           
+        locator = "{}_{}".format(filename, r_id) 
+
+        sql = "select * from copies where resource_id='{}' and adapter_identifier='{}' and canonical = 1 limit 1".format( str(r_id), self.adapter_id)
+        other_copies = self.cursor.execute(sql).fetchall()
+        if len(other_copies) != 0:
+            print("Other copies from this adapter exist")
+            raise StorageFailedException
+
+        if sha1Hashed == checksum:
+            self.s3.Bucket(self.bucket_name).upload_file(current_path, locator)
+        else:
+            print("Checksum Mismatch")
+            raise ChecksumMismatchException
+
+        self.cursor.execute(
+            "insert into copies values ( ?,?, ?, ?, ?, ?, ?)",
+            [None, r_id, self.adapter_id,  locator, sha1Hashed, self.adapter_type, True])
+        self.conn.commit()
+
+        return locator
 
 
-    def retrieve(resource_id):
-        """
-        Given a resource id, load it from the adapter's repository
+    def retrieve(self, r_id):
+        copy_info = self.cursor.execute(
+            "select * from copies where resource_id=? and adapter_identifier=? limit 1",
+            (r_id, self.adapter_id)).fetchall()[0]
+        expected_hash = copy_info[4]
+        copy_locator = copy_info[3]
+        real_hash = copy_info[4]
 
-        Downloads file to local dir, returns path.
-        """
-        pass
+        new_location = "{}/{}".format(self.ret_dir, r_id)
+
+        if real_hash == expected_hash:
+            self.s3.Bucket(self.bucket_name).download_file(copy_locator, new_location)
+        else:
+            print("Checksum Mismatch")
+            
+        return new_location
 
     def update(resource_id, updated):
         """
@@ -129,22 +197,52 @@ class S3Adapter:
         """
         pass
 
-    def delete(resource_id):
-        """
-        delete a resource
-        """
-        pass
+    def delete(self, r_id):
+        copy_info = self.cursor.execute(
+            "select * from copies where resource_id=? and adapter_identifier=? and not canonical = 1 limit 1",
+            (r_id, self.adapter_id)).fetchall()[0]
+        expected_hash = copy_info[4]
+        locator = copy_info[3]
 
-    def get_actual_checksum(self, r_id):
+        response = self.client.delete_object(Bucket=self.bucket_name, Key=locator)
+        self.cursor.execute("delete from copies where copy_id=?",
+                            [copy_info[0]])
+        self.conn.commit()
+
+    def delete_canonical(self, r_id):
+        copy_info = self.cursor.execute(
+            "select * from copies where resource_id=? and adapter_identifier=? and canonical = 1 limit 1",
+            (r_id, self.adapter_id)).fetchall()[0]
+        expected_hash = copy_info[4]
+        locator = copy_info[3]
+
+        response = self.client.delete_object(Bucket=self.bucket_name, Key=locator)
+
+        self.cursor.execute("delete from copies where copy_id=?",
+                            [copy_info[0]])
+        self.conn.commit()
+
+    def get_actual_checksum(self, r_id, delete_after_download=True):
         """
         Return an exact checksum of a resource, not relying on the metadata db
 
         If possible, this should be done with no file I/O
-        """
-        pass
 
-    def delete_canonical(self, r_id):
-        pass
+        For S3, we need to download and checksum manually. :/
+        """
+        new_path = self.retrieve("r_id")
+
+        sha1Hash = hashlib.sha1(open(new_path,"rb").read())
+        sha1Hashed = sha1Hash.hexdigest()
+
+        if delete_after_download:
+            os.remove(new_path)
+
+        return sha1Hashed
+
+    def load_metadata(self, r_id):
+        return self.cursor.execute(
+            "select * from resources where id='{}'".format(r_id)).fetchall()
 
 
 if __name__ == '__main__':
