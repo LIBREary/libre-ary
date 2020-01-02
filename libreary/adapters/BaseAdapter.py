525fe311112b1a87ff60e628103a63438a7069d9
Ben Glick <glick@lclark.edu>
2020-01-01 23:31:19 -0600

    adding docs on adapters

--- libreary/adapters/BaseAdapter.py
+++ libreary/adapters/BaseAdapter.py
@@ -3,6 +3,11 @@ class BaseAdapter:
     Class definition for the base adapter for libre-ary resource adapters.
 
     Use this mostly as a reference for what other adapters need to implement.
+
+    An adapter allows LIBREary to save copies of digital objects 
+        to different places across cyberspace. Working with many
+        adapters in concert, one should be able do save sufficient
+        copies to places they want them.
     """
 
     def __init__(config):
@@ -14,39 +19,73 @@ class BaseAdapter:
 
     def store(resource_id):
         """
-        Given a resource id, saves resource and returns confirmation
+        Store a copy of a resource in this adapter.
+
+        Store assumes that the file is in the `dropbox_dir`. 
+        AdapterManager will always verify that this is the case.
+        
+        :param r_id - the resource to store's UUID
         """
         pass
 
     def _store_canonical(current_path, r_id):
         """
-        Ingest files as the canonical adapter. Don't run this function. Ingester will call it.
+            Store a canonical copy of a resource in this adapter.
+
+            If we're using an adapter as a canonical adapter, we need
+            to be able to store from a current path, taking in a generated UUID,
+            rather than looking info up from the database.
+
+            :param current_path - current path to object
+            :param r_id - UUID of resource you're storing
+            :param checksum - checksum of resource
+            :param filename - filename of resource you're storing
+
         """
         pass
 
     def _delete_canonical(self, r_id):
+        """
+        Delete a canonical copy of a resource from this adapter.
+        Delete the corresponding entry in the `copies` table.
+        
+        :param r_id - the resource to retrieve's UUID
+        """
         pass
 
 
     def retrieve(resource_id):
         """
-        Given a resource id, load it from the adapter's repository
+        Retrieve a copy of a resource from this adapter.
 
-        Downloads file to local dir, returns path.
+        Retrieve assumes that the file can be stored to the `output_dir`. 
+        AdapterManager will always verify that this is the case.
+
+        Returns the path to the resource.
+
+        May overwrite files in the `output_dir`
+        
+        :param r_id - the resource to retrieve's UUID
         """
         pass
 
     def update(resource_id, updated):
         """
-        Overwrite the remote resource specified with what's passed into :param updated.
+        Update a resource with a new object. Preserves UUID and all other metadata (levels, etc.)
+
+        :param r_id - the UUID of the object you'd like to update
+        :param updated_path - path to the contents of the updated object.
+
         """
         pass
 
     def delete(resource_id):
         """
-        delete a resource
-        """
-        pass
+        Delete a copy of a resource from this adapter.
+        Delete the corresponding entry in the `copies` table.
+        
+        :param r_id - the resource to retrieve's UUID
+        """        pass
 
     def get_actual_checksum(self, r_id):
         """
