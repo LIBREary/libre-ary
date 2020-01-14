389aa96fc5745c75421b7eaebe715d1162a3a318
Ben Glick <glick@lclark.edu>
2020-01-13 22:10:12 -0600

    Update version.py

--- libreary/version.py
+++ libreary/version.py
@@ -1,5 +1,5 @@
 """Set module version.
 <Major>.<Minor>.<maintenance>[alpha/beta/..]
-Alphas will be numbered like this -> 0.4.0a0
+Alphas will be numbered like this -> 0.4.0-a0
 """
 VERSION = '0.0.1-rc4'
