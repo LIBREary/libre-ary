d59d8898d815bc911ff7c9b7abbbd739badca33e
Ben Glick <glick@lclark.edu>
2020-01-14 15:08:18 -0600

    add logging to init.py

--- libreary/adapters/__init__.py
+++ libreary/adapters/__init__.py
@@ -1,7 +1,7 @@
-
 from libreary.adapters.local import LocalAdapter
 from libreary.adapters.s3 import S3Adapter
 from libreary.adapters.drive import GoogleDriveAdapter
+
 
 __all__ = ['S3Adapter',
            'LocalAdapter',
