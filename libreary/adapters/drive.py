18146dcf82f5b82b3a094c8d13ace9b8fbb46391
Ben Glick <glick@glick.cloud>
2020-01-25 12:58:39 -0800

    removing errant imports from drive adapter

--- libreary/adapters/drive.py
+++ libreary/adapters/drive.py
@@ -1,9 +1,6 @@
 import os
-from shutil import copyfile
 import hashlib
-import json
 import pickle
-import io
 from pathlib import Path
 import logging
 
@@ -20,7 +17,7 @@ else:
     _google_enabled = True
 
 from libreary.exceptions import ResourceNotIngestedException, ChecksumMismatchException, NoCopyExistsException, OptionalModuleMissingException
-from libreary.exceptions import RestorationFailedException, AdapterCreationFailedException, AdapterRestored, StorageFailedException, ConfigurationError
+from libreary.exceptions import RestorationFailedException, AdapterCreationFailedException, AdapterRestored, StorageFailedException
 
 # Google Drive Scope
 SCOPES = ['https://www.googleapis.com/auth/drive']
@@ -306,6 +303,8 @@ class GoogleDriveAdapter():
         done = False
         while done is False:
             status, done = downloader.next_chunk()
+            if not status:
+                raise ChecksumMismatchException
 
     def update(self, r_id: str, updated: str) -> None:
         """
