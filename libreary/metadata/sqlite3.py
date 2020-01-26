04c56d451eae3d25827e3e92c026d0c6412ac8d7
Ben Glick <glick@glick.cloud>
2020-01-26 13:52:19 -0800

    flake8 fixes

--- libreary/metadata/sqlite3.py
+++ libreary/metadata/sqlite3.py
@@ -234,6 +234,6 @@ class SQLite3MetadataManager(object):
         :param search_term - a string with which to search against the metadata db.
             Can match UUID, filename, original path, or description.
         """
-        search_term = "%"+search_term+"%"
+        search_term = "%" + search_term + "%"
         return self.cursor.execute(f"select * from resources where name like ? or path like ? or uuid like ? or descripton like ?",
                                    (search_term, search_term, search_term, search_term)).fetchall()
