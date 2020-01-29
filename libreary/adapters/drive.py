d439ebe5354d610e249b2d79a3794149bd6f6a4c
Ben Glick <glick@lclark.edu>
2020-01-29 12:08:30 -0800

    adding adapter tests

--- libreary/adapters/drive.py
+++ libreary/adapters/drive.py
@@ -11,7 +11,7 @@ try:
     from google.auth.transport.requests import Request
     from apiclient.http import MediaFileUpload, MediaIoBaseDownload
 
-except ImportError:
+except ImportError as e:
     _google_enabled = False
 else:
     _google_enabled = True
